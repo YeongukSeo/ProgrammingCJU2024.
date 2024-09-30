08522104357ef557c9235659804d37f336f350de
YeongukSeo <seoyu0207@naver.com>
2024-09-30 20:06:02 +0900

    report#1_1

--- Peport#1_1/Report#1_1.cpp
+++ Peport#1_1/Report#1_1.cpp
@@ -3,13 +3,16 @@
 int main()
 {
 	double angle = 0.0;
+	int a = 0;
 
 	printf("각도를 입력하시오 : ");
 
 	scanf_s("%lf", &angle);
 
+
 	angle = angle + 180;
-	angle = angle - 360 * ((int)(angle / 360));
+	a = angle / 360;
+	angle = angle - 360 * a;
 	angle = angle - 180;
 	
 	printf("변환된 각도는 %0.1lf[deg] 입니다.\n\n", angle);
